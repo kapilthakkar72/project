491b63ecd08c7cb6e292afe947504da01a75f12a
kapilthakkar72 <kapilthakkar.72@gmail.com>
2016-02-17 19:54:12 +0530

    Added linear regression code

--- library/linear_regression.py
+++ library/linear_regression.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 '''
-This function takes 3 arguments:
+This function takes 5 arguments:
 x_series: independent variable
 y_series: dependent variable : y = f(x)
 param: Defines what to be treated as anomaly depending on its value as follows:
         0: Values going out of range, both with positive and negative error
         1: Values with potitive errors
         -1: Values with negative errors
-
+default_threshold: Whether to use default threshold used by system using MOD test or user defined threshold
+threshold: Threshold value if it is used defined and default_threshold is 'False'
 
 returns Following tuple:
 
@@ -27,7 +28,7 @@ returns Following tuple:
 
 Requirements: Length of both the series should be equal
 '''
-def linear_regression(x_series, y_series, param = 0):
+def linear_regression(x_series, y_series, param = 0, default_threshold = True, threshold = 0):
     # Create linear regression object
     regr = linear_model.Lars()
     # Train the model using the training sets
@@ -53,8 +54,11 @@ def linear_regression(x_series, y_series, param = 0):
             diff.append(x)
     
     # Finding outliers
+    if(default_threshold == True):
+        outVal=  1.4826*numpy.median(numpy.array(diff))
+    else:
+        outVal = threshold
     
-    outVal=  1.4826*numpy.median(numpy.array(diff))
     results = []
     for i in range(0,len(diff)):
         if(diff[i][4] > outVal):
